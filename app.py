1f4af2d2302de2be282d49778f0ff1a1f835ec71
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-07-04 06:39:29 +0530

    its working

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+import os
 from flask import Flask, request, render_template, send_file
 import numpy as np
-from PIL import Image
-import io
 import matplotlib.pyplot as plt
-from kmeans_compression import compress_image_kmeans
+import io
+from kmeans_compression import startCompressing
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/compress', methods=['POST'])
 def compress_image():
@@ -17,18 +19,29 @@ def compress_image():
         return 'No image uploaded', 400
 
     file = request.files['image']
-    image = Image.open(file.stream)
-    image_array = np.array(image)
 
-    k = 16  # Number of colors for K-means compression
-    compressed_image_array = compress_image_kmeans(image_array, k)
-    
-    compressed_image = Image.fromarray(compressed_image_array)
+    image_path = "temp.png"
+    file.save(image_path)
+
+    original_image = plt.imread(image_path)
+
+    os.remove(image_path)
+
+    print("The shape of the image is: ", original_image.shape)
+
+    X_img = np.reshape(original_image, (-1, original_image.shape[2]))
+
+    compressed_image_array = startCompressing(X_img)
+
+    X_recovered = np.reshape(compressed_image_array, original_image.shape)
+    compressed_image = X_recovered.astype('uint8')
+
     img_io = io.BytesIO()
-    compressed_image.save(img_io, 'PNG')
+    plt.imsave(img_io, compressed_image, format='PNG')
     img_io.seek(0)
 
     return send_file(img_io, mimetype='image/png')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- kmeans_compression.py
+++ kmeans_compression.py
@@ -1,2 +1,55 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+def nearest_centroid(X , centroid):
+  k = centroid.shape[0]
+  c = np.zeros(X.shape[0] , dtype=int)
+
+  for i in range(X.shape[0]):
+    distance = []
+    for j in range(k):
+      norm_ij = np.linalg.norm(X[i] - centroid[j])
+      distance.append(norm_ij)
+    c[i] = np.argmin(distance)
+  return c
+
+def compute_centroid(X , c , k):
+  m , n = X.shape
+  centroids = np.zeros((k , n))
+  for i in range(k):
+    centroids[i] = np.mean(X[c == i] , axis = 0)
+  return centroids
+
+def initialize_centroid(X , k):
+  random_index = np.random.permutation(X.shape[0])
+  return X[random_index[:k]]
+
+
+def run_kMeans(X, initial_centroids, iter):
+
+    m, n = X.shape
+    k = initial_centroids.shape[0]
+    centroids = initial_centroids
+    idx = np.zeros(m)
+
+    for i in range(iter):
+
+        print("K-Means iteration %d/%d" % (i, iter-1))
+
+        idx = nearest_centroid(X, centroids)
+
+        centroids = compute_centroid(X, idx, k)
+    
+    return centroids, idx
+
+def startCompressing(X_img):
+    k = 16
+    iter = 10
+    initial_centroids = initialize_centroid(X_img, k)
+    centroids, idx = run_kMeans(X_img, initial_centroids, iter)
+    
+    c = nearest_centroid(X_img , centroids)
+    X_recovered = centroids[c, :]
+    
+    return X_recovered
+
